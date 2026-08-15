eec47c7ed9c6e96242e628805bbc2a28a50b2817
Rushi Patil <phrishi555@gmail.com>
2026-08-15 15:49:49 -0400

    video add-responsive

--- app.js
+++ app.js
@@ -74,3 +74,45 @@ dashboard.addEventListener("mousemove", function(e) {
         icon.classList.toggle('fa-bars', !isOpen);
         icon.classList.toggle('fa-xmark', isOpen);
     });
+
+
+
+const video = document.getElementById('myVideo');
+const playBtn = document.getElementById('playBtn');
+const playIcon = document.getElementById('playIcon');
+const pauseIcon = document.getElementById('pauseIcon');
+const muteBtn = document.getElementById('muteBtn');
+const volIcon = document.getElementById('volIcon');
+const muteIcon = document.getElementById('muteIcon');
+const fullscreenBtn = document.getElementById('fullscreenBtn');
+
+playBtn.addEventListener('click', () => {
+  if (video.paused) {
+    video.play();
+    playIcon.style.display = 'none';
+    pauseIcon.style.display = 'block';
+    playBtn.style.height = "20px";
+    playBtn.style.width = "20px";
+    muteBtn.style.display = "none";
+    fullscreenBtn.style.display = "none";
+
+  } else {
+    video.pause();
+    playIcon.style.display = 'block';
+    pauseIcon.style.display = 'none';
+  }
+});
+
+muteBtn.addEventListener('click', () => {
+  video.muted = !video.muted;
+  volIcon.style.display = video.muted ? 'none' : 'block';
+  muteIcon.style.display = video.muted ? 'block' : 'none';
+});
+
+fullscreenBtn.addEventListener('click', () => {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else if (video.webkitRequestFullscreen) {
+    video.webkitRequestFullscreen();
+  }
+});
