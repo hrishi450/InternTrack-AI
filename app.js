f0a786837649d555eb4aef1fe044481541fa499e
Rushi Patil <phrishi555@gmail.com>
2026-08-18 16:15:41 -0400

    MENU BTN & VIDEO DEMIO JS

--- app.js
+++ app.js
@@ -29,7 +29,44 @@ dashboard.addEventListener("mousemove", function(e) {
 //     });
 
 
+// MENU  btn 
 
+
+let menuBtn = document.querySelector('#menuBtn');
+let nav = document.getElementById('nav');
+let logo = document.querySelector('#logo');
+let login = document.querySelector('#navlogin');
+let xBar = document.querySelector('#xBar');
+let logoText = document.querySelector('.text');
+let content = document.querySelector('#navLinks');
+
+
+
+     menuBtn.addEventListener('click',(det) => {
+        nav.classList.add('active');
+      if((det.target.parentElement)){
+        logo.style.display = 'none';
+        logoText.style.display = 'none';
+        menuBtn.style.display = 'none';
+        xBar.style.display = 'flex';
+        content.style.display ='flex';
+        login.style.width = '35px';
+      } 
+
+    } );
+      xBar.addEventListener('click',()=>{
+         nav.classList.remove('active');
+            menuBtn.style.display ='flex'; 
+            xBar.style.display = 'none'; 
+            logo.style.display = 'flex';
+            logoText.style.display = 'flex';
+          content.style.display = 'none';
+          login.style.display = 'none';
+      });
+
+
+
+// VIDEO SECTION
 const video = document.getElementById('myVideo');
 const playBtn = document.getElementById('playBtn');
 const playIcon = document.getElementById('playIcon');
@@ -39,6 +76,22 @@ const volIcon = document.getElementById('volIcon');
 const muteIcon = document.getElementById('muteIcon');
 const fullscreenBtn = document.getElementById('fullscreenBtn');
 
+let demo = document.querySelector('.btn2');
+
+      demo.addEventListener('click', (dets)=>{
+          video.play();
+    if (video.play) {
+    playIcon.style.display = 'none';
+    pauseIcon.style.display = 'block';
+    playBtn.style.height = "20px";
+    playBtn.style.width = "20px";
+    muteBtn.style.height = "20px";
+    muteBtn.style.width = "20px";
+    fullscreenBtn.style.height = "20px";
+    fullscreenBtn.style.width = "20px";
+  } 
+    
+});
 playBtn.addEventListener('click', () => {
   if (video.paused) {
     video.play();
